5421e910d99eb1f42575cf078f9a54bb2f396342
Olu Olumuyiwa <olumuyiwaolu@gmail.com>
2024-09-16 15:42:40 +0100

    Changed details of 'my services' Added graphics and UI/Ux

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
+
+        let tabLinks = document.getElementsByClassName("tab-links")
+        let tabContents = document.getElementsByClassName("tab-content")
+
+        function openTab(tabname){
+            for(tabLink of tabLinks){
+                tabLink.classList.remove("active-link")
+            }
+            for(tabContent of tabContents){
+                tabContent.classList.remove("active-tab")
+            }
+            event.currentTarget.classList.add("active-link")
+            document.getElementById(tabname).classList.add("active-tab")
+        }
+
+        let sideMenu = document.getElementById("sidemenu");
+        function openMenu(){
+            sideMenu.style.right = "0";
+        }
+        function closeMenu(){
+            sideMenu.style.right = "-200px";
+        }
+
+
+    // script for CONTACT US section
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyBgB1u1AUGjfusLmA6wbnDNJzEE9OgvsqNQQ7YwHJb16qBYZSxSct9e3PBk5Mbvf7QZQ/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            }, 5000);
+            form.reset()
+
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+
+
+
+
 //  function to submit a form to google sheet 
 //  click on extensions, select script, rename title, 
 // clear the function and replace with the code below
